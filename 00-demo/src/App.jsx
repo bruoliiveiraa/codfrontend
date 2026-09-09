3854f52e665b434e09fb5bd9bfe45a355dd5617a
bruoliiveiraa <brualmeida1120@gmail.com>
2026-09-09 17:07:14 -0300

    trabalho com react

---
+++ 00-demo/src/App.jsx
@@ -0,0 +1,44 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="cont-app">
+      <h1>Trabalhando com React</h1>
+      <h2>Onde posso trampar como dev</h2>
+      <p>
+        Empresas de Tecnologia e Software Houses: Empresas focadas em desenvolvimento sob demanda, criação de plataformas SaaS (Software as a Service) ou produtos digitais próprios. É um dos melhores ambientes para ganhar maturidade técnica rapidamente.
+      </p>
+      <p>
+        Startups: Ambientes dinâmicos onde o desenvolvedor júnior/técnico assume responsabilidades variadas, desde o front-end até a integração de rotas e banco de dados.
+      </p>
+      <p>
+        Departamentos de TI/Desenvolvimento em Grandes Empresas: Setores como varejo, instituições financeiras, saúde, logística e redes de ensino mantêm times internos para criar e sustentar seus próprios portais, sistemas corporativos e APIs.
+      </p>
+      <p>
+        Agências Digitais e Web: Focadas em criação de sites, portais, e-commerces e aplicações web institucionais para terceiros.
+      </p>
+      <p>
+        Setor Público: Oportunidades via concursos públicos técnicos para cargos de suporte/desenvolvimento de sistemas web em prefeituras, universidades e órgãos governamentais.
+      </p>
+
+      <h2>Trabalhando com tecnologia mas sem programação</h2>
+      <p>
+        Gostar de tecnologia sem ter afinidade com código não é um obstáculo. O mercado de TI é gigante e depende fortemente de funções que focam em pessoas, processos, design, dados e negócios.
+      </p>
+
+      <ul>
+        <li>Product Management & Negócios</li>
+        <li>Design e Experiência do Usuário (UI/UX)</li>
+        <li>Gestão de Projetos e Agilidade</li>
+        <li>Dados e Inteligência de Negócio</li>
+        <li>Vendas Técnicas e Sucesso do Cliente</li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default App
