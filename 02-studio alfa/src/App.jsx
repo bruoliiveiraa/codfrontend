03cc3f127aa042a6ae9266b0c07b9bb8293119ff
BRUNA OLIVEIRA DE ALMEIDA <bruna_o_de-almeida@estudante.sesisenai.org.br>
2026-09-23 17:06:10 -0300

    atividade

---
+++ 02-studio alfa/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <main></main>
+      <footer></footer> */}
+    </>
+  );
+}
+
+export default App;
---
+++ 02-studio alfa/src/components/header/header.jsx
@@ -0,0 +1,28 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">
+          <img
+            className="logo-icon"
+            src="../../../src/assets/imgs/Alfa-Romeo-Logo.png"
+            alt="Logo"
+          />
+          <span className="logo-text">Studio Alfa</span>
+        </div>
+        <nav className="nav">
+          <a href="#">Inicio</a>
+          <a href="#">Servicios</a>
+          <a href="#">Sobre</a>
+          <a href="#" className="btn-contatos">
+            Contato
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
